0ad3948e92ffe109b6de77b7f4e2f359467d98ef
堀 扶 <89004@ad.exa-corp.co.jp>
2018-12-26 09:41:49 +0900

    setup.py修正

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+setup(
+    name = 'donkeypart_game_controller',
+    version = '1.0.0',
+    author = 'Tasuku Hori',
+    author_email = 'tasuku-hori@exa-corp.co.jp',
+    url = 'https://github.com/coolerking/donkeypart_game_controller',
+    install_requires = ['docopt', 'donkeypart_bluetooth_game_controller', 'time', 'evdev'],
+    extras_require = {
+        'check_js': ['fcntl', 'strict']
+    }
+)
